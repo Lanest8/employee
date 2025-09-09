5eb38f4f93e488ab7cfdf3177cd0ae5fe3fa06d8
Lanest <lanest.lin@oocl.com>
2025-09-09 21:01:20 +0800

    refactor: move should_return_company_when_get_company_with_id_exist and should_return_company_when_get_company_with_limit

--- src/test/java/com/example/employee/CompanyControllerTest.java
+++ src/test/java/com/example/employee/CompanyControllerTest.java
@@ -42,24 +42,6 @@ public class CompanyControllerTest {
                 .andExpect(jsonPath("$[0].address").value(expect.address()));
     }
 
-    @Test
-    void should_return_create_company_when_post() throws Exception {
-        String requestBody = """
-                {
-                    "name":"oocl",
-                    "address":"珠海"
-                }
-                """;
-        MockHttpServletRequestBuilder request = post("/companies")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBody);
-
-        mockMvc.perform(request)
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.name").value("oocl"))
-                .andExpect(jsonPath("$.address").value("珠海"));
-    }
 
     @Test
     void should_return_company_when_get_company_with_id_exist() throws Exception {
@@ -91,6 +73,25 @@ public class CompanyControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(5));
 
+    }
+
+    @Test
+    void should_return_create_company_when_post() throws Exception {
+        String requestBody = """
+                {
+                    "name":"oocl",
+                    "address":"珠海"
+                }
+                """;
+        MockHttpServletRequestBuilder request = post("/companies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody);
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("oocl"))
+                .andExpect(jsonPath("$.address").value("珠海"));
     }
 
 }
